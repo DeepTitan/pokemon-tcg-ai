1d791c2213e257c1b3ac4a81886e618848439760
Isaiah Williams <williamsbyronik@gmail.com>
2026-09-04 12:10:14 -0500

    [PATCH] Use skull for poison condition marker

--- src/tracker/TrackerApp.tsx
+++ src/tracker/TrackerApp.tsx
@@ -208,9 +208,7 @@ function EventIcon({ kind, size = 17, text }: { kind: TrackerEventKind; size?: n
 function SpecialConditionIcon({ condition, size, className = 'special-condition-icon' }: { condition: string; size?: number; className?: string }) {
   const props = { className, viewBox: '0 0 32 32', 'aria-hidden': true, focusable: false, width: size, height: size };
   switch (condition) {
-    case 'Poisoned': return <svg {...props}>
-      <path d="M1 17.2c3.7 0 3.1 6.1 6.4 6.1 3.8 0 2.2-10 6.1-10 3.6 0 2 8.4 5.5 8.4 3.3 0 1.9-7 5.3-7 3.2 0 1.9 6.5 6.7 6.5V32H1Z" />
-    </svg>;
+    case 'Poisoned': return <Skull className={className} size={size} weight="fill" aria-hidden />;
     case 'Burned': return <svg {...props}>
       <path d="M16.8 2.5c1.3 5.4-3 7.1-1.1 11.2 1.1-1.9 2.7-3.7 5.3-5-.4 4.1 5.3 6.3 5.3 12.8 0 5-4.5 8.8-10.3 8.8S5.7 26.5 5.7 21.5c0-4.2 2.4-7.8 6.8-10.7-.4 3.7 1.2 6 3.4 7.6-.5-5 4.2-8.3.9-15.9Z" />
     </svg>;
